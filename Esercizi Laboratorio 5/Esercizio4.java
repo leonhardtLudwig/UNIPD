73c4513aa85340ec3ab1f65e10b3c0ef32360418
Leonardo Luigi Pepe <leo2852.info@gmail.com>
2020-11-13 10:49:44 +0100

    Esercizio5 integrato nell'esercizio 4

--- Esercizi Laboratorio 5/Esercizio4.java
+++ Esercizi Laboratorio 5/Esercizio4.java
@@ -3,7 +3,11 @@ import java.util.Scanner;
 public class Esercizio4 {
     public static void main(String args[]) {
         Scanner t = new Scanner(System.in);
-        String s = t.nextLine();
+        final int N_PAROLE = 3;
+        String s= "";
+        for(int i = 0; i< N_PAROLE; i++){
+            s= s+" "+t.nextLine();
+        }
         for (int i = s.length() - 1; i >= 0; i--) {
             System.out.print(s.charAt(i));
         }
